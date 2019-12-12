dc795c129c97f9cb9be7743d5ed00e27a56399dc
ppintogbm <ppinto@gbm.net>
2019-12-12 10:36:22 -0500

    DB Tests

--- database/create.sh
+++ database/create.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #clear
 echo "Creando la base de datos y la tabla..."
-sudo -u db2inst1 db2 create db test
-sudo -u db2inst1 db2 connect to test
-sudo -u db2inst1 db2 "CREATE TABLE OPER01
+sudo -u db2inst1 bash -c ' db2 create db test'
+sudo -u db2inst1 bash -c ' db2 connect to test'
+sudo -u db2inst1 bash -c ' db2 "CREATE TABLE OPER01
 (HORA_TRANS timestamp not null,
  OPERANDO1  decimal(12,2) not null,
  OPERANDO2  decimal(12,2) not null,
  OPERADOR varchar(5) not null,
  Primary Key (HORA_TRANS))"
-echo "Verificando la informacion creada..."
-sudo -u db2inst1 db2 connect to test
-sudo -u db2inst1 db2 list tables
-sudo -u db2inst1 db2 describe table db2inst1.oper01
-sudo -u db2inst1 db2 disconnect test
+echo "Verificando la informacion creada..."''
+sudo -u db2inst1 bash -c ' db2 connect to test'
+sudo -u db2inst1 bash -c ' db2 list tables'
+sudo -u db2inst1 bash -c ' db2 describe table db2inst1.oper01'
+sudo -u db2inst1 bash -c ' db2 disconnect test'
