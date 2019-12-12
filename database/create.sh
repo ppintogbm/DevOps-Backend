57ede130b4ee417e815fa5383ba1eb8f4b096fa4
ppintogbm <ppinto@gbm.net>
2019-12-12 10:20:12 -0500

    DB corrections

--- database/create.sh
+++ database/create.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-su - db2inst1
-clear
+#clear
 echo "Creando la base de datos y la tabla..."
-db2 create db test
-db2 connect to test
-db2 "CREATE TABLE OPER01
+su - db2inst1 -c 'db2 create db test'
+su - db2inst1 -c 'db2 connect to test'
+su - db2inst1 -c 'db2 "CREATE TABLE OPER01
 (HORA_TRANS timestamp not null,
  OPERANDO1  decimal(12,2) not null,
  OPERANDO2  decimal(12,2) not null,
  OPERADOR varchar(5) not null,
  Primary Key (HORA_TRANS))"
-echo "Verificando la informacion creada..."
-db2 connect to test
-db2 list tables
-db2 describe table db2inst1.oper01
-db2 disconnect test
+echo "Verificando la informacion creada..."'
+su - db2inst1 -c 'db2 connect to test'
+su - db2inst1 -c 'db2 list tables'
+su - db2inst1 -c 'db2 describe table db2inst1.oper01'
+su - db2inst1 -c 'db2 disconnect test'
