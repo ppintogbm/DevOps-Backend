b72f444213da1bf26b00cba7323de3e690309633
ppintogbm <ppinto@gbm.net>
2020-01-07 18:19:22 -0500

    Test on IBM Cloud with Buildah

--- ibmcloud/build.sh
+++ ibmcloud/build.sh
@@ -2,11 +2,11 @@
 container=$(buildah from ibmcom/ace)
 
 #Copy Bar file
-buildah copy --chown 1000:1001 $container ../compiled.bar /home/aceuser/bars
+buildah copy --chown 1000:1001 $container compiled.bar /home/aceuser/bars
 
 #Copy Configs
-buildah copy --chown 1000:1001 $container ../lib/db2jcc-db2jcc4.jar /home/aceuser/
-buildah copy --chown 1000:1001 $container ../initial-config /home/aceuser/initial-config
+buildah copy --chown 1000:1001 $container lib/db2jcc-db2jcc4.jar /home/aceuser/
+buildah copy --chown 1000:1001 $container initial-config /home/aceuser/initial-config
 
 #Deploy Bars
 buildah run --user 1000:1001 $container -- /bin/bash -c ace_compile_bars.sh
