a622ad8c1849245dae461afac19f83ce88e0e1e8
ppintogbm <ppinto@gbm.net>
2020-01-27 15:28:24 -0500

    Including visualizer.

---
+++ visualizer/src/main/java/com/gbm/sample/visualizer/services/OperacionService.java
@@ -0,0 +1,12 @@
+package com.gbm.sample.visualizer.services;
+
+import com.gbm.sample.visualizer.entities.Operacion;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface OperacionService{
+
+  Page<Operacion> findAllPageable(Pageable pageable);
+
+}
